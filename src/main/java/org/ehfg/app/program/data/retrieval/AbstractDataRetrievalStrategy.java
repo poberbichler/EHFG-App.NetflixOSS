63faa2ddea1a51ea0ffb9528c033fd92368b0281
Patrick Oberbichler <patrick.oberbichler@gmail.com>
2016-11-27 15:36:17 +0100

    added possibility to load program from classpath resources

--- src/main/java/org/ehfg/app/program/data/retrieval/AbstractDataRetrievalStrategy.java
+++ src/main/java/org/ehfg/app/program/data/retrieval/AbstractDataRetrievalStrategy.java
@@ -2,9 +2,7 @@ package org.ehfg.app.program.data.retrieval;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -18,35 +16,24 @@ import javax.xml.bind.Unmarshaller;
  * @author patrick
  * @since 06.2014
  */
-@Service
 public abstract class AbstractDataRetrievalStrategy<T> {
-	private static final String URL_DELIMITER = "/";
 	private final Logger logger = LoggerFactory.getLogger(getClass());
-
-	@Value("${rss.base.url}")
-	private String baseUrl;
-
-	@Value("${rss.sufix}")
-	private String rssSufix;
 
 	private Resource dataResource;
 	private JAXBContext jaxbContext;
 
-	private final String urlSnippet;
 	private final Class<T> fetchedClazz;
 
 	/**
 	 * @param fetchedClazz class to be searched for
-	 * @param urlSnippet   snipped of the url, e.g. <code>"events"</code> or <code>"speakers"</code>
 	 */
-	protected AbstractDataRetrievalStrategy(Class<T> fetchedClazz, String urlSnippet) {
+	protected AbstractDataRetrievalStrategy(Class<T> fetchedClazz) {
 		this.fetchedClazz = fetchedClazz;
-		this.urlSnippet = urlSnippet;
 	}
 
 	@PostConstruct
 	public void afterPropertiesSet() throws Exception {
-		dataResource = new UrlResource(buildUrl());
+		dataResource = getResource();
 		jaxbContext = JAXBContext.newInstance(fetchedClazz);
 	}
 
@@ -68,24 +55,7 @@ public abstract class AbstractDataRetrievalStrategy<T> {
 	}
 
 	/**
-	 * builds the valid url, e.g.: <br>
-	 * <code>http://www.ehfg.org/feed/events/data/2015.rss</code>
-	 * <p>
-	 * leading or ending slashed will automatically be removed/added, so the result will always be a valid url
+	 * getter for the resource where the data should be feteched from (e.g. classpath or external url)
 	 */
-	private String buildUrl() {
-		StringBuilder builder = new StringBuilder(baseUrl);
-		if (!baseUrl.endsWith(URL_DELIMITER) && !urlSnippet.startsWith(URL_DELIMITER)) {
-			builder.append("/");
-		}
-
-		builder.append(urlSnippet);
-		if (!urlSnippet.endsWith(URL_DELIMITER)) {
-			builder.append(URL_DELIMITER);
-		}
-
-		builder.append("data/");
-		builder.append(rssSufix);
-		return builder.toString();
-	}
+	protected abstract Resource getResource();
 }
---
+++ src/main/java/org/ehfg/app/program/data/retrieval/url/UrlBasedDataRetrievalStrategy.java
@@ -0,0 +1,60 @@
+package org.ehfg.app.program.data.retrieval.url;
+
+import org.ehfg.app.program.data.retrieval.AbstractDataRetrievalStrategy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.stereotype.Service;
+
+import java.net.MalformedURLException;
+
+/**
+ * @author patrick
+ * @since 11.2016
+ */
+@Service
+@ConditionalOnProperty(name = "program.from.classpath", matchIfMissing = true, havingValue = "false")
+abstract class UrlBasedDataRetrievalStrategy<T> extends AbstractDataRetrievalStrategy<T> {
+	private final Logger logger = LoggerFactory.getLogger(getClass());
+	private static final String URL_DELIMITER = "/";
+
+	@Value("${rss.base.url}")
+	private String baseUrl;
+
+	@Value("${rss.sufix}")
+	private String rssSufix;
+
+	private final String urlSnippet;
+
+
+	protected UrlBasedDataRetrievalStrategy(Class<T> fetchedClazz, String urlSnippet) {
+		super(fetchedClazz);
+		this.urlSnippet = urlSnippet;
+	}
+
+	@Override
+	protected Resource getResource() {
+		try {
+			StringBuilder builder = new StringBuilder(baseUrl);
+			if (!baseUrl.endsWith(URL_DELIMITER) && !urlSnippet.startsWith(URL_DELIMITER)) {
+				builder.append("/");
+			}
+
+			builder.append(urlSnippet);
+			if (!urlSnippet.endsWith(URL_DELIMITER)) {
+				builder.append(URL_DELIMITER);
+			}
+
+			builder.append("data/");
+			builder.append(rssSufix);
+
+			return new UrlResource(builder.toString());
+		} catch (MalformedURLException e) {
+			logger.error("an error occured while instantiation class", e);
+			throw new IllegalStateException(e);
+		}
+	}
+}
